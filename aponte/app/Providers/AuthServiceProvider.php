6b74fddd94e216b2497dfb2e3cfaab385787f3f0
Ivan <itaj@discente.ifpe.edu.br>
2023-11-20 23:25:39 -0300

    Novo setup do projeto

--- aponte/app/Providers/AuthServiceProvider.php
+++ aponte/app/Providers/AuthServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-
-use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -24,8 +22,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('level',function(User $user){
-            return $user->level == 'admin';
+        Gate::define('admin', function ($user) {
+            return $user->level === 'admin';
         });
     }
 }
