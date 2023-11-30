fd946f8ba426a73d015a7e4e867dfdd03e301442
enrell <enrellsa10@proton.me>
2023-11-30 20:00:01 -0300

    refactoring grammatical errors

--- aponte/resources/views/users/edit.blade.php
+++ aponte/resources/views/users/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <p class="mb-4">Editando nivél de acesso do usuário <strong>{{ $user->name }}</strong></p>
+                    <p class="mb-4">Editando nível de acesso do usuário <strong>{{ $user->name }}</strong></p>
                     <p class="mb-4">Nível de acesso atual: <strong>{{ $user->level }}</strong></p>
                 </div>
 
@@ -17,7 +17,7 @@
                     <form action="{{ route('users.update', $user->id) }}" method="post">
                         @csrf
                         @method('PUT')
-                        <label for="level">Selecione o nivél</label><br>
+                        <label for="level">Selecione o nível</label><br>
                         <select name="level" required id="" class="py-1 px-8 rounded">
                             <option value="" selected disabled>Selecione uma opção</option>
                             <option value="user">Usuário comun</option>
